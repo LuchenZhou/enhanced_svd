3bdb0ecc18591326671633fac9c84dde515d2725
LuchenZhou <94824419+LuchenZhou@users.noreply.github.com>
2025-09-14 15:49:49 +0800

    Update longbench_1.sh

--- scripts/longbench_1.sh
+++ scripts/longbench_1.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
-
 model=$1
 task=$2
 attn_pattern=$3
 sparsity=$4
-
-python -u eval/LongBench/pred_saveload.py \
-    --model $model \
-    --task $task \
+python -u eval/LongBench/pred_weights.py \
+    --model $model --task $task \
     --method duo_attn \
     --attn_load_dir ${attn_pattern} \
     --sparsity $sparsity \
     --sink_size 64 \
     --recent_size 256 \
-    --compress_svd \               
-    --svd_rank 1024               
+    --compress_svd \
+    --svd_rank 1024 \
+    --compress_kv_cache \
+    --kv_max_length 10000 \
+    --kv_sliding_window 4096 \
+    --kv_sink_tokens 256 \
+    --kv_compression_ratio 0.0625
