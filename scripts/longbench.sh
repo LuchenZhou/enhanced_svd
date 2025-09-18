108f9d8cf16fa13233e7262badcfaf2f6a3a3fb7
LuchenZhou <94824419+LuchenZhou@users.noreply.github.com>
2025-09-18 14:39:10 +0800

    Update longbench.sh

--- scripts/longbench.sh
+++ scripts/longbench.sh
@@ -2,12 +2,13 @@ model=$1
 task=$2
 attn_pattern=$3
 sparsity=$4
-python -u eval/LongBench/pred0701.py \
-    --model $model --task $task \
-    --method duo_attn \
-    --attn_load_dir ${attn_pattern} \
-    --sparsity $sparsity \
-    --sink_size 64 \
-    --recent_size 256 \
-    --compress_svd \
-    --svd_rank 1024 
+python -u eval/LongBench/pred_sedpa.py \
+  --model "$model" --task "$task" \
+  --method duo_attn \
+  --attn_load_dir "$attn_pattern" \
+  --sparsity "$sparsity" \
+  --compress_svd \
+  --svd_rank 1024 \
+  --svd_eta_retr 0.98 --svd_eta_stream 0.95 \
+  --svd_skip_first_layers 8 --lambda_skip 0.5 \
+  --window_s 256 --window_m 512 --window_w 4096
